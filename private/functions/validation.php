cad71268adc58f9554a4466814c4602c2cb0c1fa
Luis Finke <luisfinke@gmail.com>
2017-11-07 13:04:04 -0500

    Add ufloat to validate_data

--- private/functions/validation.php
+++ private/functions/validation.php
@@ -79,6 +79,21 @@ function validate_data($requestdata, $fields, &$error)
 				}
 				$validated_data[$field_name] = floatval($value);
 			}
+			else if($field_type=="ufloat")
+			{
+				if(filter_var($value, FILTER_VALIDATE_FLOAT)===false)
+				{
+					$error = "invalid value ".$value." for field ".$field;
+					return null;
+				}
+				$newval = floatval($value);
+				if($newval < 0)
+				{
+					$error = "invalid value ".$value." for field ".$field;
+					return null;
+				}
+				$validated_data[$field_name] = $newval;
+			}
 			else if($field_type=="integer")
 			{
 				if(filter_var($value, FILTER_VALIDATE_INT)===false)
@@ -101,7 +116,7 @@ function validate_data($requestdata, $fields, &$error)
 					$error = "invalid value ".$value." for field ".$field;
 					return null;
 				}
-				$validated_data[$field_name] = intval($value);
+				$validated_data[$field_name] = $newval;
 			}
 			else if($field_type=="bool")
 			{
